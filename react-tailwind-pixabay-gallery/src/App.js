d182f0373ea43f6b8fec9561dd9d28b3e2739d6d
Durgesh Yadav <durgeshyadav3107@gmail.com>
2022-09-09 00:38:05 +0530

    serach functionality added..

--- react-tailwind-pixabay-gallery/src/App.js
+++ react-tailwind-pixabay-gallery/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { ImageCard } from "./components/ImageCard";
+import { ImageSearch } from "./components/ImageSearch";
 
 
 function App() {
@@ -19,12 +20,16 @@ function App() {
       })
       .catch(err => console.log(err));
 
-  }, []);
+  }, [term]);
 
 
   return (
 
     <div className="container mx-auto">
+      <ImageSearch searchText={(text) => setTerm(text)} />
+
+      {!isLoading && images.length === 0 && <h1 className="text-5xl text-center mx-auto mt-32">No Images Found Bro..</h1>}
+
       {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading..</h1>
         : <div className="grid grid-cols-3 gap-4">
 
